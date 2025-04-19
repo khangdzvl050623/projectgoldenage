2e903afff60b6d1226d9d07c273758ce738b5afd
khangkhang <vinhkhang236@gmail.com>
2025-04-19 12:59:43 +0700

    update

--- src/main/java/com/khang/goldenage/config/WebConfig.java
+++ src/main/java/com/khang/goldenage/config/WebConfig.java
@@ -4,19 +4,17 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**")
-          .allowedOrigins(
-            "http://localhost:4000",
-            "http://192.168.2.65:4000",
-            "https://goldenages.onrender.com" // thêm domain trên Render
-          )
-
-          .allowedMethods("GET", "POST", "PUT", "DELETE")
-                .allowedHeaders("*");
-    }
+  @Override
+  public void addCorsMappings(CorsRegistry registry) {
+    registry.addMapping("/api/**")
+      .allowedOrigins(
+        "http://localhost:4000", // Thường dùng cho development
+        "http://192.168.2.65:4000", // Thường dùng cho development
+        "https://goldenages.online"  // domain fe
+      )
+      .allowedMethods("GET", "POST", "PUT", "DELETE")
+      .allowedHeaders("*");
+  }
 }
